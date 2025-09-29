1637cdddeeb1d0c57be0516c66b7fa9d714f51f0
김정현 <pandaejdgus1357@hufs.ac.kr>
2025-09-29 13:44:18 +0900

    Remove previous submission (202503891)

--- submissions/202503891/assignment2/dsops/stats/labels.py
+++
@@ -1,7 +0,0 @@
-from typing import List, Dict
-
-def label_distribution(labels: List[str]) -> Dict[str, int]:
-    freq = {}
-    for label in labels:
-        freq[label] = freq.get(label, 0) + 1
-    return freq
