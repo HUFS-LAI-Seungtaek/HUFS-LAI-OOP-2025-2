143d2a217f3edcba3b9246ca16eebaa027178c8e
김정현 <pandaejdgus1357@hufs.ac.kr>
2025-09-29 13:36:17 +0900

    Submit cachekit assignment (202503891)

---
+++ cachekit/__init__.py
@@ -0,0 +1,22 @@
+VERSION = "1.0"
+
+def print_version_info():
+    print(f"cachekit version {VERSION}")
+
+class Cache:
+    def __init__(self):
+        self._data = {}
+
+    def put(self, key, value):
+        self._data[key] = value
+
+    def get(self, key, default=None):
+        return self._data.get(key, default)
+
+    def __len__(self):
+        return len(self._data)
+
+    def clear(self):
+        self._data.clear()
+
+__all__ = ["Cache", "print_version_info", "VERSION"]
