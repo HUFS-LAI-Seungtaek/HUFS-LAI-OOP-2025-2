ef1f2a619aef4eb1cfe1cc968e65b832fad3ee7e
김정현 <pandaejdgus1357@hufs.ac.kr>
2025-10-01 13:47:46 +0900

    Remove old textops from previous problem2

--- submissions/202503891/assignment2/problem2/textops/main.py
+++
@@ -1,7 +0,0 @@
- from . import clean_text, word_tokens
-
-if __name__ == "__main__":
-    s = "  Hello,   WORLD!  "
-    print(clean_text(s))                 
-    print(word_tokens(clean_text(s)))    
-
