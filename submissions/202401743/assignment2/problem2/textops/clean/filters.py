2b7c7dc4358266bf5c4a1004bd342cedc0704039
ldy56-hub <ldy56@hufs.ac.kr>
2025-10-01 13:58:03 +0900

    d

--- submissions/202401743/assignment2/problem2/textops/clean/filters.py
+++ submissions/202401743/assignment2/problem2/textops/clean/filters.py
@@ -2,12 +2,17 @@ import re
 import string
 
 def clean_text(s: str) -> str:
-   s = s.lower()
-   s = re.sub(r"\s+", " ", s)
-   s = s.strip()
-   keep = {"'", "-"}
-   remove = set(string.punctuation) - keep
+   s = s.lower() 
+   s = re.sub(r"\s+", " ", s) 
+   s = s.strip() 
+   keep = {"'", "-"} #유지할 문자 set으로 정의
+   remove = set(string.punctuation) - keep  
+   # string.punctuation : 표준 문장부호 포함 문자열  -> set으로 변환 
+   # => 제거할 문자열들만을 포함하는 remove set 생성
    punctuation_pattern = "[" + re.escape("".join(remove)) + "]"
+   # "".join() : remove내의 요소들을 연결 
+   # re.escape() : 다른 의미를 가진 문자들을 다른 문자들과 같이 평범한 문자로 취급
+   # punctuation_pattern = [제거할 문자열]
    s = re.sub(punctuation_pattern, "", s)
    s = re.sub(r"\s+", " ", s)
    s = s.strip()
