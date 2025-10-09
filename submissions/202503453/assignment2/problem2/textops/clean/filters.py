3c82e5a6f5feff179120090dc6fa6a21ea263d75
haneungyu1653 <haneungyu1653@gmail.com>
2025-10-09 20:28:01 +0900

    modified:   submissions/202503453/assignment2/problem1/main.py     modified:   submissions/202503453/assignment2/problem2/textops/__init__.py     modified:   submissions/202503453/assignment2/problem2/textops/clean/filters.py 	modified:   submissions/202503453/assignment2/problem2/textops/tokenize/word.py 	modified:   submissions/202503453/assignment2/problem3/main.py 	new file:   submissions/202503453/assignment2/problem4/dsops/__init__.py 	new file:   submissions/202503453/assignment2/problem4/dsops/main.py 	new file:   submissions/202503453/assignment2/problem4/dsops/split/__init__.py 	new file:   submissions/202503453/assignment2/problem4/dsops/split/train_test.py 	new file:   submissions/202503453/assignment2/problem4/dsops/stats/__init__.py 	new file:   submissions/202503453/assignment2/problem4/dsops/stats/labels.py 	modified:   submissions/202503453/assignment2/problem6/main.py
    	modified:   submissions/202503453/assignment2/problem4/README.md
    	modified:   submissions/202503453/assignment2/problem4/dsops/__init__.py
    	modified:   submissions/202503453/assignment2/problem4/dsops/main.py
    	modified:   submissions/202503453/assignment2/problem4/dsops/split/__init__.py
    	modified:   submissions/202503453/assignment2/problem4/dsops/split/train_test.py
    	modified:   submissions/202503453/assignment2/problem4/dsops/stats/labels.py
    	submissions/202503453/assignment2/problem5/cachekit/

--- submissions/202503453/assignment2/problem2/textops/clean/filters.py
+++ submissions/202503453/assignment2/problem2/textops/clean/filters.py
@@ -1,14 +1,7 @@
 import re
 import string
 
-def clean_text(s: str) -> str:
-    """
-    Pipeline:
-      1) lowercase
-      2) strip
-      3) collapse all whitespace to single spaces
-      4) remove ASCII punctuation except apostrophes (') and hyphens (-)
-    """
+
     # TODO: 구현하세요
     # 힌트:
     # 1) s.lower() - 소문자 변환
@@ -16,8 +9,17 @@ def clean_text(s: str) -> str:
     # 3) s.strip() - 앞뒤 공백 제거
     # 4) string.punctuation에서 특정 문자 제외하고 제거
     # 5) set 연산을 활용해서 keep = {"'", "-"}, 나머지는 제거
-    raise NotImplementedError
+    
+def clean_text(s: str) -> str:
+    s = s.lower() 
+    s = re.sub(r"\s+", " ", s)
+    s = s.strip()
+    keep = {"'", "-"}
+    remove = set(string.punctuation) - keep
+    removing = f"[{re.escape(''.join(remove))}]"
+    s = re.sub(removing, "", s)
 
+    return s
 
 if __name__ == "__main__":
     def run_tests():
@@ -26,5 +28,5 @@ if __name__ == "__main__":
         assert clean_text("...") == ""
         assert clean_text(" A  B\tC\nD ") == "a b c d"
         print("filters.py tests passed.")
-    # run_tests()
+    run_tests()
     pass
