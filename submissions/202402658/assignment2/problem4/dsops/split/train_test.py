35fdd2d44c9738840a060b431b9362d35b2f7a47
bampora <kunhue9@gmail.com>
2025-10-10 18:38:54 +0900

    revised problem4

--- submissions/202402658/assignment2/problem4/dsops/split/train_test.py
+++ submissions/202402658/assignment2/problem4/dsops/split/train_test.py
@@ -3,13 +3,17 @@ import random
 
 def train_test_split(seq: list, test_ratio: float, seed: int | None = None) -> tuple[list, list] :
     """
-    cut the input sequence into train and test set.
+    main work: cut the input sequence into train and test set.
     test ratio must be within [0.0 , 1.0]
     keep the original sequence aside safely, and use a copy for shuffling.
-    if there's seed on input, shuffle with that seed. else shuffle randomly.
+    if there's a seed on input, shuffle with that seed. else shuffle randomly.
     """
+
     if not 0.0 <= test_ratio <= 1.0 :
         raise ValueError('ValueError')
+
+    if len(seq) == 0 :
+        return [], []
 
     copy = seq[:]    #원본 보존, shuffle할 copy생성.
 
@@ -21,11 +25,3 @@ def train_test_split(seq: list, test_ratio: float, seed: int | None = None) -> t
     train , test = copy[:cut_idx] , copy[cut_idx:]
 
     return train , test
-
-
-
-
-
-
-
-if __name__ == "__main__":
--- submissions/202402658/assignment2/problem4/dsops/stats/labels.py
+++ submissions/202402658/assignment2/problem4/dsops/stats/labels.py
@@ -1,15 +1,11 @@
-"""
-
-"""
 
 
 def label_distributuion(labels: list[str]) -> dict[str, int]:
-
-
-
-
-    return 
-
-
-
-if __name__ == "__main__":
+    """
+    입력 list를 순회하며 label별 빈도를 기록한 dictionary 반환
+    """
+    d={}
+    for label in lables:
+        d[label] = d.get(label,0) + 1
+    
+    return d
