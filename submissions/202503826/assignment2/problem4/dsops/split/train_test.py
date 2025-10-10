5557377a22756d89951e78fb7785f128b3ea41ef
hw0523 <raywonny0163@naver.com>
2025-10-10 14:10:03 +0900

    problem 4 수정

--- submissions/202503826/assignment2/problem4/dsops/split/train_test.py
+++ submissions/202503826/assignment2/problem4/dsops/split/train_test.py
@@ -2,6 +2,8 @@ import random
 
 def train_test_split(seq: list, test_ratio: float, seed: int | None = None) -> tuple[list, list]:
     copy_seq = seq.copy()
+    if not seq:
+        return [], []
     if seed is not None:
         random.seed(seed)
     random.shuffle(copy_seq)
