42f4966e158b5ba8e8394667ec8c8312404ba65b
bampora <kunhue9@gmail.com>
2025-10-10 19:12:57 +0900

    Update problem5 main.py
    demo에 VERSION 변수 import 수정.

--- submissions/202402658/assignment2/problem5/cachekit/main.py
+++ submissions/202402658/assignment2/problem5/cachekit/main.py
@@ -1,7 +1,7 @@
 #demo
 
 if __name__ == "__main__":
-    from . import Cache, print_version_info
+    from . import Cache, print_version_info , VERSION
     print_version_info()
     c = Cache(); c.put("a", 1)
     print(len(c), c.get("a"))   # 1 1
